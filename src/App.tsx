da76d321d5f82d4e40eae6c483bb4a0254b912f6
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-07-12 22:46:13 -0700

    fade animations

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,26 @@ import styled from "styled-components";
 
 import { Fade } from "./animations/Fade";
 
- 
-
 function App() {
-
- 
-
   return (
     <>
-    <h1>Framer Motion</h1>
+      <h1>Framer Motion</h1>
 
-
- 
-<Fade direction="in">
-<Box/>
- 
-</Fade>
+      <h2>Fade with slide</h2>
+      <Fade direction="in" slideEntrance>
+        <Box />
+      </Fade>
     </>
   );
 }
 
 const Box = styled.div`
-  background-color: red;
+  background-color: #222c3f;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  border-radius: 10px;
   width: 100px;
   height: 100px;
-`
+`;
 
 export default App;
--- src/animations/Fade.tsx
+++ src/animations/Fade.tsx
@@ -1,34 +1,30 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-
-
 interface IProps {
-  direction: "in" | "out"
-  children: React.ReactNode
+  direction: "in" | "out";
+  slideEntrance?: boolean;
+  children: React.ReactNode;
 }
 
-export const Fade: React.FC<IProps> = ({direction, children}) => {
-  
-
+export const Fade: React.FC<IProps> = ({
+  direction,
+  children,
+  slideEntrance,
+}) => {
   const animation = {
     initial: {
-      opacity: direction === "in" ? 0 : 1
+      opacity: direction === "in" ? 0 : 1,
+      y: slideEntrance ? -20 : undefined,
     },
     animate: {
       opacity: direction === "in" ? 1 : 0,
+      y: slideEntrance ? 0 : undefined,
     },
     transition: {
-      duration: 1// in seconds
-    }
-  }
+      duration: 1, // in seconds
+    },
+  };
 
-  
-  
-  return (
-    <motion.div {...animation}>
-      {children}
-    </motion.div>
-  );
+  return <motion.div {...animation}>{children}</motion.div>;
 };
- 
